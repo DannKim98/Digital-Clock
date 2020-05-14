1dcd3f59f51768733fe7ce1008d80443cbf6aa64
DannKim <denis.kim@nu.edu.kz>
2020-05-14 22:08:44 +0600

    clock

---
+++ setTime.js
@@ -0,0 +1,24 @@
+function setTime(){ 
+	let fullDate = new Date();
+
+	let hours = fullDate.getHours();
+	let minutes = fullDate.getMinutes();
+	let seconds = fullDate.getSeconds();
+
+	if(seconds < 10){
+	seconds = `0${seconds}`;
+	}
+
+	if(minutes < 10){
+	minutes = `0${minutes}`;
+	}
+
+	if(hours < 10){
+	hours = `0${hours}`;
+}
+
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+}
+setInterval(setTime, 1000);
